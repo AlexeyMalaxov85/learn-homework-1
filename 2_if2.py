18e223217473424882c0300ee51321d6c9e77eca
AlexeyMalaxov85 <94935244+AlexeyMalaxov85@users.noreply.github.com>
2021-11-27 22:35:17 +0300

    первый раз не правильно добавил

--- 2_if2.py
+++ 2_if2.py
@@ -14,28 +14,6 @@
   и выводя на экран результаты
 
 """
-#first_string = input("")
-#second_string = input("")
-
-
-#def main(first_string, second_string):
-#  if isinstance(first_string, str) and isinstance(second_string, str):
-#    return True
-#  else:
-#    return False
-
-
-#decision = main(first_string, second_string)
-
-#if decision == True and len(first_string) == len(second_string):
-#  print("1")
-#elif decision == True and len(first_string) > len(second_string):
-#  print("2")    
-#elif second_string == "learn":
-#  print("3")
-#else:
-#  print("0")
-
 def main(first_str, second_str):
   if isinstance(first_str, str) and isinstance(second_str, str):
     if len(first_str) == len(second_str):
@@ -46,9 +24,6 @@ def main(first_str, second_str):
       return 2
   else:
     return 0
-
-
-
 
 if __name__ == "__main__":
   print(main(1, "съешь ещё этих мягких французских булок"))
